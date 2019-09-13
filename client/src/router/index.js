36e66fae0a2b6cdf117aaaef3acbbe81129d0c85
Gino Louwsma <ginolouwsma01@gmail.com>
2019-09-13 12:18:57 +0200

    Added the registration page with the needed client-side validation

---
+++ client/src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Index from '@/components/Index'
+
+import Register from '@/components/Register'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'Index',
+      component: Index
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    }
+  ]
+})
