6c44410b902b87c137248f0f6b91e46f45e1f2d9
Gino Louwsma <ginolouwsma01@gmail.com>
2019-09-17 10:04:10 +0200

    The client side also shows validation errors received from our server end

--- server/validator/index.js
+++ server/validator/index.js
@@ -10,16 +10,16 @@ const RULES = {
     },
 
     "min": (value, args) => {
-        return [value.length >= Number(args[0]), `value requires a length of minimal ${args[0]} characters`]
+        return [value.length >= Number(args[0]), `The value requires a length of minimal ${args[0]} characters`]
     },
 
     "max": (value, args) => {
-        return [value.length <= Number(args[0]), `value requires a length of maximal ${args[0]} characters`]
+        return [value.length <= Number(args[0]), `The value requires a length of maximal ${args[0]} characters`]
     },
 
     "email": (value, args) => {
         let result = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/gm.exec(value)
-        return [result != null]
+        return [result != null, "The value must be a valid e-mail address"]
     },
 
     "word": (value, args) => {
