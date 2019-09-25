f5e2f34636586fbdce7623f5ba20eb88eb5184ed
Gino Louwsma <ginolouwsma01@gmail.com>
2019-09-25 12:50:38 +0200

    Made some changes to the server api

--- client/src/views/Reset.vue
+++ client/src/views/Reset.vue
@@ -132,7 +132,7 @@ export default {
 
       if (this.errors.length === 0) {
         try {
-          let request = await axios.post(`${location.protocol}//${location.hostname}:` + process.env.SERVER_PORT + '/api/reset', {
+          let request = await axios.post(`${location.protocol}//${location.hostname}` + process.env.DEV ? '' : (':' + process.env.SERVER_PORT) + '/api/reset', {
             token: this.$route.params.token,
             new_password: this.password,
             new_password_confirmation: this.password_confirmation
