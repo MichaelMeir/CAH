f772f6e471469d9a66f182096235f090aec6d563
michaelmeir <99044520@mydavinci.nl>
2019-10-03 09:18:57 +0200

    added user getter from rooms in the socket handler

--- server/socket/handler.js
+++ server/socket/handler.js
@@ -10,6 +10,12 @@ module.exports = {
         "sendMessage"
     ],
     
+    /**
+     * @method ping
+     * @description Socket method to check if user is still connected to websocket using a timeout
+     * 
+     * @param {Object} meta contains util data
+     */
     ping: (meta) => {
         const TIMEOUT = 1000 * 5
         if(meta.timeout) {
@@ -38,26 +44,46 @@ module.exports = {
         return [1]
     },
 
-    joinRoom(meta, jwt, id) {
+    /**
+     * @method joinRoom
+     * @description joins user socket to room if accesible and not full
+     * 
+     * @param {Object} meta contains util data
+     * @param {String} jwt jwt token to authenticate user to check if client is eligable to join a room
+     * @param {String} roomId String containing a room id, checked in 'rooms' if exists
+     * 
+     * @yields {Object} with 'room' containing roomId, if null then joining a room failed
+     */
+    joinRoom(meta, jwt, roomId) {
         return User(jwt, () => {
             if(err || !user) {
                 return {room: null, err: err, authenticated: false}
             }
-            id = id.toLowerCase()
-            if(user && rooms[id]) {
-                if(rooms[id].users.length < rooms[id].maxUsers) {
-                    meta.room = id
-                    rooms[id].users.push(user.uuid)
+            roomId = roomId.toLowerCase()
+            if(user && rooms[roomId]) {
+                if(rooms[roomId].users.length < rooms[roomId].maxUsers) {
+                    meta.room = roomId
+                    rooms[roomId].users.push(user.uuid)
+                    rooms[roomId].usernames.push(user.username_withcase)
                     meta.emit((emitMeta) => {
                         emitMeta.methods.sendMessage(user.username_withcase + " joined the game room!") // chat when user joins room
                     })
-                    return {room: id}
+                    return {room: roomId}
                 }
             }
             return {room: null}
         }, meta.db, meta.models, meta.ip)
     },
 
+    /**
+     * @method leaveRoom
+     * @description leaves user socket from room, notifies other users that the user has left
+     * 
+     * @param {Object} meta contains util data
+     * @param {String} jwt jwt token to authenticate user to check if client is eligable to join a room 
+     * 
+     * @yields {Object} containing 'room' with null regardless of data sent
+     */
     leaveRoom(meta, jwt) {
         return User(jwt, (user, err) => {
             if(err || !user) {
@@ -75,11 +101,21 @@ module.exports = {
                     emitMeta.methods.sendMessage(user.username_withcase + " Left the game room!") // chat when user leaves room
                 })
                 rooms[meta.room].users.filter(i => i !== user.uuid)
+                rooms[meta.room].usernames.filter(i => i !== user.username_withcase)
             }
             return {room: null}
         }, meta.db, meta.models, meta.ip)
     },
 
+    /**
+     * @method createRoom
+     * @description creates a room with owner as user requesting the new room
+     * 
+     * @param {Object} meta contains util data
+     * @param {String} jwt jwt token to authenticate user to check if client is eligable to join a room
+     * 
+     * @yields {Object} with 'room' containing roomId, if null then joining a room failed
+     */
     createRoom(meta, jwt) {
         return User(jwt, () => {
             if(err || !user) {
@@ -100,6 +136,7 @@ module.exports = {
                     rooms[code] = {
                         owner: user.uuid,
                         users: [user.uuid],
+                        usernames: [user.username_withcase],
                         maxUsers: 10,
                     }
                     return {room: code}
@@ -107,5 +144,19 @@ module.exports = {
             }
             return {room: null}
         }, meta.db, meta.models, meta.ip)
-    }
+    },
+
+    /**
+     * @method getUsers
+     * @description gives usernames of a specific room when requested
+     * 
+     * @param {Object} meta contains util data
+     * @param {String} roomId String containing a room id, checked in 'rooms' if exists
+     */
+    getUsers(meta, roomId) {
+        if(rooms[roomId]) {
+            return rooms[roomId].usernames
+        }
+        return []
+    },
 }
